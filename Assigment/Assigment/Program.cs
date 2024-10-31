d0dc774a4064c9c87372d0c984910612bd47580b
Muhammad Faham <fahambbs@gmail.com>
2024-10-31 01:26:36 -0700

    Add files via upload

---
+++ Assigment/Assigment/Program.cs
@@ -0,0 +1,26 @@
+﻿using Assigment.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birth Year:");
+            int birthYear = Convert.ToInt32(Console.ReadLine());
+
+            Calculate_Age agecalculator = new Calculate_Age(birthYear);
+            int Age = agecalculator.Agecalculate();
+
+            Console.WriteLine("Your Age is:" + Age);
+            Console.ReadLine();
+
+        }
+    }
+   
+}
---
+++ Assigment/Assigment/Properties/Calculate Age.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment.Properties
+{
+    internal class Calculate_Age
+    {
+        private int birthYear;
+        
+        public Calculate_Age(int birthYear)
+        {
+            this.birthYear = birthYear;
+        }
+        public int Agecalculate()
+        {
+            int currentYear = DateTime.Now.Year;
+            return currentYear - birthYear;
+        }
+
+    }
+}
