a41ad6566f56e52c6ba3d69fd2b356282f3b8f64
Muhammad Faham <fahambbs@gmail.com>
2024-11-16 12:15:01 -0800

    Add files via upload

---
+++ Assigments/Properties/Books.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigments.Properties
+{
+    internal class Books
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+
+        public Books(string title, string author, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+        }
+    }
+}
---
+++ Assigments/Properties/LibraryManagement.cs
@@ -0,0 +1,59 @@
+﻿using LibraryManagement;
+using System.Collections.Generic;
+using System;
+
+ario 1: Library Management System
+csharp
+Copy code
+using System;
+using System.Collections.Generic;
+
+namespace LibraryManagement
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public bool IsAvailable { get; set; } = true;
+    }
+
+    class Library
+    {
+        private List<Book> book = new List<Book>();
+
+        public void AddBook(Book book) => book.Add(book);
+
+        public void RemoveBook(string isbn) =>
+            book.RemoveAll(b => b.ISBN == isbn);
+
+        public bool IsBookAvailable(string isbn) =>
+            book.Exists(b => b.ISBN == isbn && b.IsAvailable);
+    }
+
+    class Member
+    {
+        public string Name { get; set; }
+
+        public void BorrowBook(Library library, string isbn)
+        {
+            if (library.IsBookAvailable(isbn))
+            {
+                var book = library.Book.Find(b => b.ISBN == isbn);
+                book.IsAvailable = false;
+                Console.WriteLine($"{Name} borrowed \"{book.Title}\".");
+            }
+            else
+                Console.WriteLine($"The book with ISBN {isbn} is not available.");
+        }
+
+        public void ReturnBook(Library library, string isbn)
+        {
+            var book = library.Book.Find(b => b.ISBN == isbn);
+            if (book != null && !book.IsAvailable)
+            {
+                book.IsAvailable = true;
+                Console.WriteLine($"{Name} returned \"{book.Title}\".");
+            }
+        }
+    }
