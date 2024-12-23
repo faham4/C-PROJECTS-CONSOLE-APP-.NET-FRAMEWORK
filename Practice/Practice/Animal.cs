0e6066fc00ef23248903b6cc9b0de0183984e9f8
Muhammad Faham <fahambbs@gmail.com>
2024-12-23 00:52:11 -0800

    Add files via upload

---
+++ Practice/Practice/Animal.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal interface Animal
+    {
+        void Eat();
+        void Sleep();
+        void Move();
+        void MakeSound();
+
+    }
+    //Tiger01
+    public class Tiger : Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Tiger is eating buffalo.");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("Tiger is Sleeping.");
+        }
+        public void Move()
+        {
+            Console.WriteLine("The tiger prowls silently through the jungle.");
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine("The tiger roars loudly.");
+        }
+    }
+    //Parrot02
+    public class Parrot : Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("The parrot eats seeds and fruits.");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("Parrots sleep in nests.");
+        }
+        public void Move()
+        {
+            Console.WriteLine("Parrot perch while moving with agility");
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine("Parrots chirp, squawk, mimic sounds, and even imitate human speech.");
+        }
+    }
+    //shark03
+    public class Shark : Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("The shark eat fish, squid, seals, and marine creatures, using sharp teeth.");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("The shark rest while swimming, some need water flowing over gills.");
+        }
+        public void Move()
+        {
+            Console.WriteLine("The shark swims swiftly through the ocean.");
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine(" The shark are silent; they communicate through body language, not sounds.");
+        }
+    }
+
+
+}
---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal tiger = new Tiger();
+            Animal parrot = new Parrot();
+            Animal shark = new Shark();
+
+            //Method calls and classes
+            //Tiger01
+            Console.WriteLine("Tiger Behavior:");
+            tiger.Eat();
+            tiger.Sleep();
+            tiger.Move();
+            tiger.MakeSound();
+         
+
+
+            //Method calls and classes
+            //Parrot02
+            Console.WriteLine("\n Parrot Behavior:");
+            parrot.Eat();
+            parrot.Sleep();
+            parrot.Move();
+            parrot.MakeSound();
+            
+
+            //Method calls and classes
+            //Shark03
+            Console.WriteLine("\n Shark Behavior:");
+            shark.Eat();
+            shark.Sleep();
+            shark.Move();
+            shark.MakeSound();
+        
+        }
+    }
+}
